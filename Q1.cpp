39698cb99b9f90e4e3d6d1a772a0d54fe8984af3
Ansley Peduru <ansleypeduru96@gmail.com>
2015-02-02 21:29:03 -0500

    Multiples of 3 and 5

---
+++ Q1.cpp
@@ -0,0 +1,20 @@
+//Multiples of 3 and 5
+//Problem 1
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	for (int x = 1; x < 1000; x++) 
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+			sum += x;
+	}
+	cout << "The sum of all multiples of 3 or 5 below 1000 is " << sum ;
+	return 0;
+}
