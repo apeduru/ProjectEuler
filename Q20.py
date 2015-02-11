7f64fd33c1331024ff8ec4f632d23599ad218b7e
Ansley Peduru <ansleypeduru96@gmail.com>
2015-02-10 21:30:51 -0500

    Factorial digit sum

--- Q20.py
+++ Q20.py
@@ -1,7 +1,7 @@
-import math
+from math import factorial
 
 sum = 0 
-for x in str(math.factorial(100)):
+for x in str(factorial(100)):
 	sum += int(x)
 print "The sum of the digits of the number 2^1000 is ", sum  	
 
