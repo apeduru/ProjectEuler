75af98b312f75901e02fffe66b1809a95698855b
apeduru <ansleypeduru96@gmail.com>
2015-07-08 18:33:20 -0400

    Power digit sum

--- 016.py
+++ 016.py
@@ -1,4 +1,9 @@
+'''
+2^15 = 32768 and the sum of its digits is 3+2+7+6+8=26
+What is the sum of the digits of the number 2^1000?
+'''
+
 sum = 0
 for x in str(pow(2,1000)):
-	sum += int(x)
-print(sum)
+    sum += int(x)
+print "The sum of the digits is", sum
